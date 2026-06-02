eed7e07842eb31ffd5f8aa89d9a6524d2934206b
Francisco Ochoa <lukariXU01@gmail.com>
2026-06-02 13:52:53 -0400

    Add: print para process

--- include/process.h
+++ include/process.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <iostream>
+
 enum State {
     READY = 0,
     RUNNING = 1,
@@ -29,5 +31,12 @@ struct Process {
     int totalTime;
 
     Process(int _id, int _arrivalTime, int _cpu, int _io, int _priority);
-};
 
+    void print() const {
+        std::cout << " [Process Created] ID: " << id 
+                << " | Arrival: " << arrivalTime 
+                << " | CPU Burst: " << initialTimeCPU 
+                << " | I/O Burst: " << initialTimeIO 
+                << " | Priority: " << priority << "\n";
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
 #include "process.h"
 
 int main() {
     std::cout << "Simulador de Planificacion de Procesos Inicializado" << std::endl;
+    Process p1(1, 0, 5, 3, 1);
+    p1.print();
     return 0;
 }
